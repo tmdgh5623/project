431432257ec263025784b2de58d4546f7be1a291
tmdgh5623 <tmdgh5623@gmail.com>
2025-09-03 11:17:11 +0900

    Delete api/debug.js

--- api/debug.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-  const keys = ['GOOGLE_PROJECT_ID','GOOGLE_SERVICE_EMAIL','GOOGLE_PRIVATE_KEY'];
-  const result = {};
-  for (const k of keys) result[k] = !!process.env[k];
-  res.status(200).json(result);
-};
